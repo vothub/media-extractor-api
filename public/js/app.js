3c5964a4f241cca567316c6039b012178b3ec5c8
Vot <vot@siliconfen.co>
2018-04-12 23:50:11 +0100

    Added on-page audio and video players

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,57 @@
     }
   });
 })();
+
+function playAudioOnPage(url) {
+  if (!url) {
+    return console.log('No URL provided - aborting playAudioOnPage');
+  }
+  console.log('playAudioOnPage triggered with URL', url);
+
+  var audio = document.getElementById('audio');
+  if (!audio) {
+    return console.log('No audio tag present on page - aborting playAudioOnPage', url);
+  }
+
+  stopVideoOnPage();
+  stopAudioOnPage();
+
+  var source = document.createElement('source');
+  source.setAttribute('src', url);
+  audio.appendChild(source);
+  audio.play();
+  audio.className = '';
+}
+
+function playVideoOnPage(url) {
+  if (!url) {
+    return console.log('No URL provided - aborting playVideoOnPage');
+  }
+  console.log('playVideoOnPage triggered with URL', url);
+
+  var video = document.getElementById('video');
+  if (!video) {
+    return console.log('No video tag present on page - aborting playVideoOnPage', url);
+  }
+
+  stopVideoOnPage();
+  stopAudioOnPage();
+
+  var source = document.createElement('source');
+  source.setAttribute('src', url);
+  video.appendChild(source);
+  video.play();
+  video.className = '';
+}
+
+function stopAudioOnPage() {
+  var audio = document.getElementById('audio');
+  audio.pause();
+  audio.className = 'hidden';
+}
+
+function stopVideoOnPage() {
+  var video = document.getElementById('video');
+  video.pause();
+  video.className = 'hidden';
+}
